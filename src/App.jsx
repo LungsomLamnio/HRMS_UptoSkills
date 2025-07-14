e40658497b004c4ef27e0d2e473eb2cbdae4f0e4
Lungsom Lamnio <145887275+LungsomLamnio@users.noreply.github.com>
2025-07-14 17:25:40 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import { Routes, Route } from "react-router-dom";
-import Sidebar from "./components/SideBar";
-import Events from "./components/Event3";
+import Sidebar from "./components/Sidebar";
 import "/src/App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Event2 from "./components/Event2";
+import Event1 from "./components/Event1";
+import Event3 from "./components/Event3";
 
 function App() {
   return (
-    <div className="d-flex" style={{ height: "100vh", width: "100vw" }}>
+    <div className="d-flex">
       <Sidebar />
-      
-      <div className="main-content">
+      <div className="flex-grow-1 p-4">
         <Routes>
-          <Route path="/event" element={<Events />} />
+          <Route path="/event" element={<Event1 />} />
+          <Route path="/event/schedule" element={<Event2 />} />
+          <Route path="/event/confirmation" element={<Event3 />} />
         </Routes>
       </div>
     </div>
