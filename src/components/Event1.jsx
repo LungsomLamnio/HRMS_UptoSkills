b95ff174ac2d3ae97b973ffc72859decb2b355e2
Kaushik Mandal <koushik369mondal@gmail.com>
2025-07-15 21:23:15 +0530

    fix: correct formatting of time strings and remove unused time slots in Event1 component

--- src/components/Event1.jsx
+++ src/components/Event1.jsx
@@ -22,7 +22,7 @@ export default function Event1() {
       image: profile1,
       duration: "30 min",
       timeZone: "Asia/Kolkata",
-      time:'03:00 - Tue Jul 15 2025'
+      time: '03:00 - Tue Jul 15 2025'
 
     },
     {
@@ -39,7 +39,7 @@ export default function Event1() {
 
   const times = [
     "12:00", "12:30", "13:00", "13:30", "14:00", "14:30",
-    "15:00", "15:30", "16:00", "16:30", "17:00", "17:30",
+    "15:00", "15:30", "16:00", "16:30"
   ];
 
   const handleSchedule = () => {
